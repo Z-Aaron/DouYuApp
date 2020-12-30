5822721d671648e492f0618835b14dc0f2c4d22d
Z-Aaron <215167354@qq.com>
2020-12-30 09:40:38 +0800

    导入kingfisher

--- DouYuApp/Main/View/CollectionViewNormalCell.swift
+++ DouYuApp/Main/View/CollectionViewNormalCell.swift
@@ -7,11 +7,18 @@
 
 import UIKit
 
-class CollectionViewNormalCell: UICollectionViewCell {
+class CollectionViewNormalCell: CollectionBaseCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    //MARK:- 控件属性
+    @IBOutlet weak var roomNameLabel: UILabel!
+    
+    //MARK:- 定义模型属性
+    override var anchor :AnchorsModel?{
+        didSet{
+            super.anchor = anchor
+            //3.房间名称
+            roomNameLabel.text = anchor?.room_name
+        }
     }
 
 }
